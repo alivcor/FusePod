de961926df40422e60d5ff4e7559b7440f8f687a
alivcor <abhinandandubey@live.com>
2020-08-07 23:24:04 -0400

    added basic nucleobases

--- main.py
+++ main.py
@@ -2,15 +2,39 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from enum import Enum
 
+class Nucleobase(Enum):
+    A = "Adenine"
+    C = "Cytosine"
+    G = "Guanine"
+    T = "Thymine"
+    U = "Uracil"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+pairings = {
+    Nucleobase.A: (Nucleobase.U, Nucleobase.T),
+    Nucleobase.C: (Nucleobase.G),
+    Nucleobase.G: (Nucleobase.C),
+    Nucleobase.T: (Nucleobase.A),
+    Nucleobase.U: (Nucleobase.A)
+}
+
+class Nucleotide(object):
+    def __init__(self, nucleobase, phosphate = "mono"):
+        self.nucleobase = nucleobase
+        self.phosphate = phosphate
+
+    def canPair(self, nucleotide):
+        return nucleotide.nucleobase in pairings[self.nucleobase]
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    adenine = Nucleotide(Nucleobase.A)
+    guanine = Nucleotide(Nucleobase.G)
+    uracil = Nucleotide(Nucleobase.U)
+    print(adenine.canPair(guanine))
+    print(adenine.canPair(uracil))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
